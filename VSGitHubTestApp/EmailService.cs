71c5ca5aac9debcb01603116cc63803511a236cc
Sunkari Rakesh <sunkari.rakesh@bpmlinks.com>
2021-04-28 12:01:57 +0000

    sync

---
+++ VSGitHubTestApp/EmailService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VSGitHubTestApp
+{
+    class EmailService
+    {
+
+        public void sendEmail()
+        {
+            Console.WriteLine("Email Service added");
+            Console.WriteLine("Gmail notificatin sent");
+        }
+    }
+}
