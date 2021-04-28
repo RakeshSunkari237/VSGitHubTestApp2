b3179518d1ea620fa95a8c50c6da65e868f90aba
Sunkari Rakesh <sunkari.rakesh@bpmlinks.com>
2021-04-28 11:31:42 +0000

    Add project files.

---
+++ VSGitHubTestApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VSGitHubTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
